bea1d6c620f060e36efb2e06332e54f3b5a90d45
trisha-n <trisha2006mummy@gmail.com>
2020-07-24 15:45:22 +0530

    added smoke

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gameState ="onSling"
 
 
 function preload() {
@@ -69,16 +70,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState === "onSling"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
